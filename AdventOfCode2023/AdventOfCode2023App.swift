c23655ed4e4df8810e4960a9ff12c049841c3884
Pavel Stepanov <pavel@tomorrow.one>
2024-01-16 19:35:06 +0100

    Create empty project

---
+++ AdventOfCode2023/AdventOfCode2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventsOfCode2023App.swift
+//  AdventsOfCode2023
+//
+//  Created by Pavel Stepanov on 16.01.24.
+//
+
+import SwiftUI
+
+@main
+struct AdventsOfCode2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
